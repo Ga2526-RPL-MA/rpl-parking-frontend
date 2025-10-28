572ca710d6c9323aad854190b8b23311e6ee6210
Ardyansyah3103 <ardyansyah6859@gmail.com>
2025-10-28 23:52:09 +0700

    feat(login): add user token to sessionStorage

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,12 @@
 // src/hooks/useAuth.ts
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
-
 import { authLoginApi } from "../utils/api/authLoginApi";
+// import { useRouter } from "next/navigation";
 
 export function useAuth() {
+  // const router = useRouter();
+
   const loginMutation = useMutation({
     mutationFn: async ({
       email,
@@ -16,9 +18,17 @@ export function useAuth() {
       return await authLoginApi.login(email, password);
     },
 
-    onSuccess: (data) => {
-      if (typeof window !== "undefined" && data?.token) {
-        localStorage.setItem("token", data.token);
+    onSuccess: (res) => {
+      console.log("Login Response:", res);
+
+     const token = res?.data?.token;
+     const user = res?.data?.user;
+     
+     if (token) {
+        sessionStorage.setItem("token", token);
+     }
+      if (user) {
+         sessionStorage.setItem("user", JSON.stringify(user));
       }
 
       toast.success("Login berhasil 🎉", {
